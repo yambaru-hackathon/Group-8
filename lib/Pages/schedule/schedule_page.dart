9c0029c3c58a6153990fbd318498ddfdf8e8d2c2
Minmal <mkt.ishigaki@gmail.com>
2024-02-21 22:13:34 +0900

    ファイル更新

--- lib/Pages/schedule/schedule_page.dart
+++ lib/Pages/schedule/schedule_page.dart
@@ -3,6 +3,11 @@ import 'package:table_calendar/table_calendar.dart';
 
 class SchedulePage extends StatelessWidget {
   DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime foucusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   const SchedulePage({super.key});
   @override
@@ -14,20 +19,27 @@ class SchedulePage extends StatelessWidget {
   }
 
   Widget content() {
-    return Column(
-      children: [
-        Text("123"),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+        Text("Selected Day ="+ today.toString().split(" ")[0]),
         Container(
           child: TableCalendar(
             locale: "en_US",
             rowHeight: 43,
-            headerStyle: HeaderStyle(formatButtonVisible: false),
+            headerStyle:
+                HeaderStyle(formatButtonVisible: false, titleCentered: true),
+            availableGestures: AvailableGestures.all,
+            selectedDayPredicate: (day) ==> isSameDay(day, today),
             focusedDay: today,
             firstDay: DateTime.utc(2010, 01, 01),
             lastDay: DateTime.utc(2030, 01, 01),
+            onDaySelected: _onDaySelected,
           ),
         ),
       ],
+      ),
     );
   }
 }
