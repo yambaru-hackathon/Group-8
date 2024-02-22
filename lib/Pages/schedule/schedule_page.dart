69cbe209e00db493069b22c69f5a774e0a6bd1d6
Minmal <mkt.ishigaki@gmail.com>
2024-02-22 20:13:55 +0900

    イベントUIの追加

--- lib/Pages/schedule/schedule_page.dart
+++ lib/Pages/schedule/schedule_page.dart
@@ -1,45 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:group8_app/Pages/schedule/calendar.dart';
 
 class SchedulePage extends StatelessWidget {
-  DateTime today = DateTime.now();
-  void _onDaySelected(DateTime day, DateTime foucusedDay) {
-    setState(() {
-      today = day;
-    });
-  }
-
   const SchedulePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Schedule')),
-      body: content(),
-    );
-  }
-
-  Widget content() {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-        Text("Selected Day ="+ today.toString().split(" ")[0]),
-        Container(
-          child: TableCalendar(
-            locale: "en_US",
-            rowHeight: 43,
-            headerStyle:
-                HeaderStyle(formatButtonVisible: false, titleCentered: true),
-            availableGestures: AvailableGestures.all,
-            selectedDayPredicate: (day) ==> isSameDay(day, today),
-            focusedDay: today,
-            firstDay: DateTime.utc(2010, 01, 01),
-            lastDay: DateTime.utc(2030, 01, 01),
-            onDaySelected: _onDaySelected,
-          ),
-        ),
-      ],
-      ),
     );
   }
 }
