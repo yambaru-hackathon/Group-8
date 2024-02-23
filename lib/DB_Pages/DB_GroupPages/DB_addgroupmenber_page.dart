241c2cdffc312516f3e1ce9e4e87742a55b9cad0
YamashiroAyumu <ayumu0716.1111@gmail.com>
2024-02-24 02:02:45 +0900

    DB用ファイルに必須パッケージの追加

---
+++ lib/DB_Pages/DB_GroupPages/DB_addgroupmenber_page.dart
@@ -0,0 +1,2 @@
+import 'package:cloud_firestore/cloud_firestore.dart';  // firebaseに接続するパッケージ
+import 'package:flutter/material.dart';
--- lib/DB_Pages/DB_GroupPages/DB_group_page.dart
+++ lib/DB_Pages/DB_GroupPages/DB_group_page.dart
@@ -6,7 +6,7 @@ class DB_group_page_class {
   final db = FirebaseFirestore.instance;                // dbの初期化
 
   Future<void> readGroupSearch(value) async {           // 絞り込み検索のread関数
-    final snapshot = await db.collection('GroupID')
+    final snapshot = await db.collection('Group')
       .where('name', isEqualTo: value)                  // DBにあるグループ名と一致する場合のみ
       .orderBy('authority')                             // 名前順
       .limit(10)                                        // 10だけ表示
@@ -14,7 +14,7 @@ class DB_group_page_class {
 
     // 見つかったグループを全部つなげて文字にする
     final docs = snapshot.docs.map(
-      (doc) => doc.data().toString(),
+      (doc) => doc.data().toString(),                     // (doc) => doc.data()['取りたいカラム'].toString(),
     );
     final GroupID = docs.join();
     if(GroupID.isEmpty == false){                         // GroupIDが空じゃなかったら
