01cf78862d31e3393e9e85734290f8f34d58fb2e
nakayuu803 <ynakamura704@gmail.com>
2024-02-24 11:43:06 +0900

    グループ画面遷移完了

--- lib/Pages/GroupPages/groupdetail_page.dart
+++ lib/Pages/GroupPages/groupdetail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:goup8_app/DB_Pages/DB_GroupPages/DB_group_page.dart';
+import 'package:goup8_app/Pages/GroupPages/group_page.dart';
+import 'package:goup8_app/Pages/GroupPages/selectperson_page.dart';
 
 class NewGroupDetail extends StatefulWidget {
   const NewGroupDetail({Key? key}) : super(key: key);
@@ -9,107 +13,172 @@ class NewGroupDetail extends StatefulWidget {
 
 class _NewGroupDetailState extends State<NewGroupDetail> {
   int? _selectedPermission;
+  bool _selectperson = false;
+  final focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          '新しいグループを作成',
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.group),
-                  iconSize: 50,
+    return Focus(
+      focusNode: focusNode,
+      child: GestureDetector(
+        onTap: focusNode.requestFocus,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              '新しいグループを作成',
+            ),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.group),
+                      iconSize: 50,
+                    ),
+                    Flexible(
+                      child: TextField(
+                        maxLength: 20,
+                        maxLines: 1,
+                        decoration: const InputDecoration(
+                          hintText: "グループ名を入力",
+                          labelText: "Group Name",
+                        ),
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.search,
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          // テスト検索部分
+                          if (value.isEmpty != true) {
+                            // エンターキー押したとき文字列が空じゃないなら
+                            final DB_group_page =
+                                DB_group_page_class(); // DB_group_pageのDB_group_page_classを参照
+                            DB_group_page.readGroupSearch(
+                                value); // DB_group_pageのreadGroupSearch(value)関数を実行
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                const Flexible(
-                  child: TextField(
-                    maxLength: 20,
-                    maxLines: 1,
-                    decoration: InputDecoration(
-                      hintText: "グループ名を入力",
-                      labelText: "Group Name",
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 20,
+                  left: 20,
+                ),
+                child: const Text(
+                  'グループ編集の権限',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              RadioListTile<int>(
+                title: const Text('全ての人に許可'),
+                value: 1,
+                groupValue: _selectedPermission,
+                dense: true,
+                onChanged: (int? value) {
+                  setState(() {
+                    _selectedPermission = value;
+                    _selectperson = false;
+                  });
+                },
+              ),
+              RadioListTile<int>(
+                title: const Text('許可しない'),
+                value: 2,
+                groupValue: _selectedPermission,
+                dense: true,
+                onChanged: (int? value) {
+                  setState(() {
+                    _selectedPermission = value;
+                    _selectperson = false;
+                  });
+                },
+              ),
+              RadioListTile<int>(
+                title: const Text('許可する人を選択する'),
+                value: 3,
+                groupValue: _selectedPermission,
+                dense: true,
+                onChanged: (int? value) {
+                  setState(() {
+                    _selectedPermission = value;
+                    _selectperson = true;
+                  });
+                },
+              ),
+              Visibility(
+                visible: _selectperson,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                          onPressed: _selectedPermission == 3
+                              ? () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SelectPerson(),
+                                    ),
+                                  );
+                                }
+                              : null,
+                          icon: const Icon(
+                            Icons.add_circle_outline,
+                            size: 50,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const Text(
+                          'add person',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Expanded(child: SizedBox.shrink()),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(300, 50),
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Creat',
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(
-              top: 20,
-              left: 20,
-            ),
-            child: const Text(
-              'グループ編集の権限',
-              style: TextStyle(
-                fontSize: 20,
               ),
-            ),
-          ),
-          RadioListTile<int>(
-            title: const Text('全ての人に許可'),
-            value: 1,
-            groupValue: _selectedPermission,
-            dense: true,
-            onChanged: (int? value) {
-              setState(() {
-                _selectedPermission = value;
-              });
-            },
-          ),
-          RadioListTile<int>(
-            title: const Text('許可しない'),
-            value: 2,
-            groupValue: _selectedPermission,
-            dense: true,
-            onChanged: (int? value) {
-              setState(() {
-                _selectedPermission = value;
-              });
-            },
-          ),
-          RadioListTile<int>(
-            title: const Text('許可する人を選択する'),
-            value: 3,
-            groupValue: _selectedPermission,
-            dense: true,
-            onChanged: (int? value) {
-              setState(() {
-                _selectedPermission = value;
-              });
-            },
-          ),
-          const Expanded(child: SizedBox.shrink()),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(300, 50),
-                backgroundColor: Colors.blue,
+              const SizedBox(
+                height: 25,
               ),
-              child: const Text(
-                'Creat',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
+            ],
           ),
-          const SizedBox(
-            height: 25,
-          )
-        ],
+        ),
       ),
     );
   }
---
+++ lib/Pages/GroupPages/selectperson_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SelectPerson extends StatelessWidget {
+  const SelectPerson({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('select person'),
+      ),
+    );
+  }
+}
